7a5c489edd4bab806574cc309d63b560ad60ba00
Uiratan Cavalcante <uiratan@gmail.com>
2024-01-04 04:16:27 -0300

    challange ok

---
+++ node/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+
+const mysql = require('mysql')
+var connection = mysql.createConnection(config)
+
+connection.query("DROP TABLE IF EXISTS people;")
+connection.query("CREATE TABLE IF NOT EXISTS people (id int not null auto_increment, name varchar(255), primary key(id));")
+connection.end()
+
+app.get('/', (req, res) => {
+    var connection = mysql.createConnection(config)
+    connection.query("INSERT INTO people(name) VALUES ('Uiratan Cavalcante')")
+
+    connection.query(
+        "SELECT * FROM people",
+        function (err, rows) {
+            if (err) throw err;
+
+            var html = "<h1>Full Cycle Rocks!</h1> <ul>"
+            for (var i in rows) html += "<li>" + rows[i].name + "</li>";
+            html += "</ul>"
+
+            res.send(html);
+        }
+    );
+
+    connection.end()
+})
+
+app.listen(port, () => {
+    console.log('Rodando na porta ' + port)
+})
